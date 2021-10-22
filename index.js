6e37d32cf64a864d916eea7c67b6c7bb3821e8bb
javi marliz <jfmarli@gmail.com>
2021-10-22 22:04:07 +0200

    🚀 RELEASE: welcome message

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-// clears the console
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Hello there`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true
+});
+
 
 console.log(
-`Javier Martinez Lizama - Software Engineer
+`
+Javier Martinez Lizama - Software Engineer
 
 
 Github:     https://github.com/jmlgh
